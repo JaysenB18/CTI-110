270dc5b2d125c1dfe8dbf63a3c214e7c75da363b
JaysenB18 <160041226+JaysenB18@users.noreply.github.com>
2024-02-14 10:36:35 -0500

    Add files via upload

---
+++ P1LAB1_Boscan_Jaysen.py
@@ -0,0 +1,9 @@
+#Jaysen Boscan
+#2/12/2024
+#P1LAB1
+#Using input and output
+#creating a variable to hold first name
+first_name = input("enter your first name: ")
+#print first name with a greeting
+print("Hey", first_name,)
+print("Welcome to Zybooks" )
