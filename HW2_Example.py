839397c359eb347618c1943e5a60a52e6fb44869
JaysenB18 <160041226+JaysenB18@users.noreply.github.com>
2024-02-19 16:32:11 +0000

    First upload

---
+++ HW2_Example.py
@@ -0,0 +1,19 @@
+#Jaysen
+#2/19/2024
+#input and outut 
+
+'''
+Pseudocode
+Get budget (int)
+get destination (str)
+get gas (int)
+get food (int)
+add expenses 
+budget - expenses 
+display results - use example 
+
+print("This program calculates and displays travel expenses")
+#get budget from user 
+budget = int(input("enetr budget: "))
+dest = input("enter your destination: ")
+'''
