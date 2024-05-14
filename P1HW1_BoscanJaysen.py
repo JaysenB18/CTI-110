9c876f51121d15b4b3da22131611970b7dc237b3
JaysenB18 <160041226+JaysenB18@users.noreply.github.com>
2024-05-14 12:07:28 -0400

    Add files via upload

---
+++ P1HW1_BoscanJaysen.py
@@ -0,0 +1,18 @@
+#Jaysen Boscan
+#2/19/2024
+#P1HW1
+#Basic input and output
+
+user_num = int(input('Enter integer :'))
+
+print ('You entered:', user_num)
+
+print (user_num, 'squared is', user_num * user_num)
+
+print('And', user_num,'cubed is', user_num * user_num * user_num, '!!')
+
+user_num2 = int(input('Enter another integer:\n'))
+
+print(user_num, "+", user_num2,'is',user_num + user_num2)
+
+print(user_num, "*", user_num2,'is',user_num * user_num2)
