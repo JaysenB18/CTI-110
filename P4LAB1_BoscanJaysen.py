fd89e7ab9203f0c8be76ccd2b9c92a0ce02da05d
JaysenB18 <160041226+JaysenB18@users.noreply.github.com>
2024-05-14 12:09:08 -0400

    Add files via upload

---
+++ P4LAB1_BoscanJaysen.py
@@ -0,0 +1,19 @@
+import turtle
+win = turtle.Screen()
+timmy = turtle. Turtle()
+#add some display options
+timmy.pensize(4)          #increase penszie(takes integer)
+timmy.pencolor("purple")  #set pencolor(takes string)
+
+#draw triangle 
+for item in range(4):
+    #actions to happen
+    timmy.forward(100)
+    timmy.left(90)
+#draw square 
+for item in range(3):
+    #actions to happen
+    timmy.forward(100)
+    timmy.left(120)
+    
+
